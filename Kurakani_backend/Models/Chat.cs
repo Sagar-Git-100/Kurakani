70070905e79212ec8205d8719e6cc18b9f2551ca
Sagar10041 <sagar10041@gmail.com>
2025-07-21 21:30:40 +0545

    chat model added

--- Kurakani_backend/Models/Chat.cs
+++ Kurakani_backend/Models/Chat.cs
@@ -9,6 +9,20 @@ namespace Kurakani_backend.Models
 {
     public class Chat
     {
-        
+         [Key]
+    public Guid ChatId { get; set; }
+
+    public bool IsGroup { get; set; } = false;
+    public string? GroupName { get; set; }
+    public string? GroupDescription { get; set; }
+    public string? GroupImageUrl { get; set; }
+
+    public Guid? CreatedBy { get; set; }
+    public User? Creator { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public ICollection<ChatParticipant>? Participants { get; set; }
+    public ICollection<Message>? Messages { get; set; }
     }
 }
