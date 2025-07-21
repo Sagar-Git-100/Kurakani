0fdd109195a69864c62522c00e3661b3fc72c091
Sagar10041 <sagar10041@gmail.com>
2025-07-21 21:27:18 +0545

    Notification model added

--- Kurakani_backend/Models/ Notification.cs
+++ Kurakani_backend/Models/ Notification.cs
@@ -9,6 +9,16 @@ namespace Kurakani_backend.Models
 {
     public class  Notification
     {
-       
+        [Key]
+    public Guid NotificationId { get; set; }
+
+    [Required]
+    public Guid UserId { get; set; }
+    public User? User { get; set; }
+
+    public string Title { get; set; } = default!;
+    public string Message { get; set; } = default!;
+    public DateTime SentAt { get; set; }
+    public bool IsRead { get; set; }
     }
 }
