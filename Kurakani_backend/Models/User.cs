297eeaf5f832e8214f13571fbd0e7a5411c20ea2
Sagar10041 <sagar10041@gmail.com>
2025-07-21 21:26:41 +0545

    User Model added

--- Kurakani_backend/Models/User.cs
+++ Kurakani_backend/Models/User.cs
@@ -8,5 +8,26 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Kurakani_backend.Models
 {
+    public class User
+    {
+        [Key]
+    public Guid UserId { get; set; }
 
+    [Phone, Required]
+    public string PhoneNumber { get; set; } = default!;
+
+    public string? Name { get; set; }
+    public string? ProfilePictureUrl { get; set; }
+    public string? About { get; set; }
+
+    public DateTime? LastSeen { get; set; }
+    public bool IsOnline { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+
+    public ICollection<ChatParticipant>? ChatParticipants { get; set; }
+    public ICollection<Message>? SentMessages { get; set; }
+    public ICollection<UserDevice>? Devices { get; set; }
+    }
 }
