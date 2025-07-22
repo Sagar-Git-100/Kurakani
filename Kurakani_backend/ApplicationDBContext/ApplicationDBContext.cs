781101fc492c74b73991cd6fc00f5139ee9bda04
Sagar-Git_100 <sagarbasnet948848@gmail.com>
2025-07-22 20:31:03 +0545

    added dbContext  file

---
+++ Kurakani_backend/ApplicationDBContext/ApplicationDBContext.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kurakani_backend.ApplicationDBContext
+{
+    public class ApplicationDBContext
+    {
+        
+    }
+}
