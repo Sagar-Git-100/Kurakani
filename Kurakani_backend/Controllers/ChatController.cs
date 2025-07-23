c3576a58b6dd32a95436e58f255c6359df5bda2c
Sagar-Git_100 <sagarbasnet948848@gmail.com>
2025-07-23 21:26:09 +0545

    added chatcontroller  file

---
+++ Kurakani_backend/Controllers/ChatController.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kurakani_backend.Controllers
+{
+    public class ChatController
+    {
+        
+    }
+}
